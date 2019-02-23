928780813b0a0a224cc16b57d8a7e362b17782fc
Tommy Lau <tlau425@yahoo.com>
2019-02-23 03:25:18 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,34 +9,57 @@ def loop_iterator(number_of_times)
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times = 1
+  loop do
+    puts phrase
+    break if number_of_times >= 7
+    number_of_times += 1
+  end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times = 1
+  while number_of_times <= 7
+  puts phrase
+  number_of_times += 1
+  end
 end
+
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times = 0
+  while number_of_times < 7
+  puts phrase
+  number_of_times += 1
+  end
 end
+
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  number_of_times = 1
+  until number_of_times > 7
+  puts phrase
+  number_of_times += 1
+  end
 end
+
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times = 1..7
+  for times in number_of_times
+  puts phrase
+  end
 end
 
